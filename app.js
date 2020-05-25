f64e5c64aa868ef1a542c46b4e967b248372c9ab
Imre Bartis <imre.bartis@siili.com>
2020-05-25 15:16:08 +0300

    handle post requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -10,11 +11,29 @@ const tours = JSON.parse(
 const port = 3000;
 
 app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({ status: 'success', results: tours.length, data: { tours } });
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
-app.post('/', (req, res) => {
-  res.send('u can post to this endpoint');
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
