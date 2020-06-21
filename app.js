9bf6ce1b3227cb81a3aa12018f2f1517549e92a6
Imre Bartis <imre.bartis@siili.com>
2020-06-21 04:10:33 +0300

    create & mount multiple routes

--- app.js
+++ app.js
@@ -133,19 +133,25 @@ const deleteUser = (req, res) => {
 };
 
 // 3. routes
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter
+  .route('/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/api/v1/users/:id')
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter
+  .route('/:id')
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4. start server
 app.listen(port, () => {
