d474c31c225268de14ccc7314ea6a2d4b94a55fa
Imre Bartis <imre.bartis@siili.com>
2020-06-23 15:32:14 +0300

    use param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID =(req, res, next, val) => {
+  console.log(`tour id is ${val}`)
+  if (req.params.id * 1 > tours.length) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+  };
+  next();
+}
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -49,13 +60,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Invalid id',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -65,13 +69,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Invalid id',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
