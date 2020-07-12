2dc427494b923ceda2c885cae648da4bcf6d28be
Imre Bartis <imre.bartis@siili.com>
2020-07-12 09:55:26 +0200

    connect the database with Express

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: `${__dirname}/config.env` });
-
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+dotenv.config({ path: `${__dirname}/config.env` });
 
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('DB connection successful');
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
